44f811ad89785b6d14cae476d7ba2d64df2c4688
Eli <eliix94pap@gmail.com>
2020-10-27 18:58:31 +0100

    destination schema

--- backend/models/schemas/destination-schema.js
+++ backend/models/schemas/destination-schema.js
@@ -5,7 +5,7 @@ module.exports = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: false },
   distance: { type: Number, required: false },
-  length: { type: Number, required: false },
+  _length: { type: Number, required: false },
   description: { type: String, required: false },
   state: { type: String, required: false, default: 'active' }
 })
