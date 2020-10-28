af335665aa720174033ea19aa4896b3789f5a64f
Eli <eliix94pap@gmail.com>
2020-10-28 22:33:20 +0100

    maquetado MissionsPage y BlogArticle

--- frontend/proyecto-spacex/src/pages/BlogArticle.vue
+++ frontend/proyecto-spacex/src/pages/BlogArticle.vue
@@ -1,9 +1,28 @@
 <template>
- <div class="container article-body">
-    <h1>{{article.title}}</h1>
-    <img :src="article.page" alt="">
-    <p>{{article.body}}</p>
- </div>
+  <div class="container sigle-article padre p-6">
+
+      <div class="card hijo">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img :src="article.image" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-content">
+              <p class="title is-4">{{article.title}}</p>
+            </div>
+          </div>
+
+          <div class="content">
+            <p>{{article.body}}</p>
+
+            <p class="mt-4">{{article.published}}</p>
+          </div>
+        </div>
+      </div>
+
+  </div>
 </template>
 
 <script>
